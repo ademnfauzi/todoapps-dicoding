34b187f5ba7aae8244ec03b7a97b5803bf9f1272
Ade Muhammad Nur Fauzi <ademnfauzi76@gmail.com>
2022-07-25 16:11:35 +0700

    update storange

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
 const todos = [];
 const RENDER_EVENT = 'render-todo';
 
+const SAVED_EVENT = 'saved-todo';
+const STORAGE_KEY = 'TODO_APPS';
+ 
+function isStorageExist() /* boolean */ {
+  if (typeof (Storage) === undefined) {
+    alert('Browser kamu tidak mendukung local storage');
+    return false;
+  }
+  return true;
+}
+
+document.addEventListener(SAVED_EVENT, function () {
+    console.log(localStorage.getItem(STORAGE_KEY));
+  });
+
 document.addEventListener('DOMContentLoaded', function () {
     const submitForm = document.getElementById('form');
     submitForm.addEventListener('submit', function (event) {
       event.preventDefault();
       addTodo();
     });
+    if (isStorageExist()) {
+        loadDataFromStorage();
+      }
   });
 
   function addTodo() {
@@ -18,6 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
     todos.push(todoObject);
    
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
   }
 
   function generateId() {
@@ -103,6 +122,7 @@ document.addEventListener('DOMContentLoaded', function () {
    
     todoTarget.isCompleted = true;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
   }
 
   function findTodo(todoId) {
@@ -121,6 +141,7 @@ document.addEventListener('DOMContentLoaded', function () {
    
     todos.splice(todoTarget, 1);
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
   }
    
    
@@ -131,6 +152,7 @@ document.addEventListener('DOMContentLoaded', function () {
    
     todoTarget.isCompleted = false;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
   }
 
   function findTodoIndex(todoId) {
@@ -141,4 +163,25 @@ document.addEventListener('DOMContentLoaded', function () {
     }
    
     return -1;
+  }
+
+  function saveData() {
+    if (isStorageExist()) {
+      const parsed = JSON.stringify(todos);
+      localStorage.setItem(STORAGE_KEY, parsed);
+      document.dispatchEvent(new Event(SAVED_EVENT));
+    }
+  }
+
+  function loadDataFromStorage() {
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+    let data = JSON.parse(serializedData);
+   
+    if (data !== null) {
+      for (const todo of data) {
+        todos.push(todo);
+      }
+    }
+   
+    document.dispatchEvent(new Event(RENDER_EVENT));
   }
